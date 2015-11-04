e2e330dec609fc76a7d7d535d90f4b67ec65db16
John Caraballo <jcaraballo019@gmail.com>
2015-11-04 12:31:07 -0400

    Initial Commit

---
+++ Stormy/MainActivity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using Android.Net;
+
+using Square.OkHttp;
+
+namespace Stormy
+{
+	[Activity (Label = "Stormy", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			string apiKey = "32b0b275eea19d641b0069e91c91b50a";
+			double latitude = 37.8267;
+			double longitude = -122.423;
+			string apiUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+			if (isNetworkAvailable ()) {
+				OkHttpClient client = new OkHttpClient ();
+				Request request = new Request.Builder ().Url (apiUrl).Build ();
+
+				Call call = client.NewCall (request);
+
+				call.Enqueue (
+					response => {
+						string body = response.Body ().String ();
+						Console.WriteLine (body);
+					}, (req, exception) => {
+						ShowError();
+				}
+				);
+			}
+			else {
+				Toast.MakeText (this, "Network unavailable", ToastLength.Long).Show ();
+			}
+
+		}
+
+		private void ShowError(){
+			AlertDialogFragment dialog = new AlertDialogFragment ();
+			dialog.Show (this.FragmentManager, "error_dialog");
+		}
+
+		private bool isNetworkAvailable() {
+			bool isAvailable = false;
+
+			ConnectivityManager manager = (ConnectivityManager) GetSystemService (Context.ConnectivityService);
+			NetworkInfo netWorkInfo = manager.ActiveNetworkInfo;
+
+			if(netWorkInfo != null && netWorkInfo.IsConnected){
+				isAvailable = true;
+			}
+
+			return isAvailable;
+		}
+	}
+}
+
+
---
+++ Stormy/src/AlertDialogFragment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace Stormy
+{
+	public class AlertDialogFragment : DialogFragment
+	{
+		public AlertDialogFragment ()
+		{
+		}
+
+		public override Dialog OnCreateDialog(Bundle savedInstanceState) {
+			Context context = (Context)Activity;
+
+			AlertDialog.Builder builder = new AlertDialog.Builder (context)
+				.SetTitle ("Oops sorry!")
+				.SetMessage ("Something has gone wrong!")
+				.SetPositiveButton("OK",delegate {
+					
+				});
+
+			AlertDialog dialog = builder.Create ();
+
+			return dialog;
+		}
+	}
+}
+
